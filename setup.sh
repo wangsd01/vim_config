3e093e2226b4ecd173dd4d6ae407408caa20e7d5
wangsd01 <wang_sd01@126.com>
2016-04-22 13:42:54 -0400

    all setup files

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "# vim_config" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/wangsd01/vim_config.git
+git push -u origin master
